d8dadd8d14c2a2d962c9fd17bb7a3b0f90cce5f0
rlly92 <reubenlimliangyi@gmail.com>
2023-06-04 00:42:21 +0800

    backend auth0 configuration in progress

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const { auth } = require("express-oauth2-jwt-bearer");
 require("dotenv").config();
+
+// middleware for Auth0:
+const checkJwt = auth({
+  audience: "https://project3bootcamp/api",
+  issuerBaseURL: `https://dev-uun7isc4ev72mwao.us.auth0.com/`,
+});
 
 // importing Routers
 const UsersRouter = require("./routers/usersRouter");
@@ -16,7 +23,7 @@ const { users } = db;
 const usersController = new UsersController(users);
 
 // inittializing Routers
-const usersRouter = new UsersRouter(usersController).routes();
+const usersRouter = new UsersRouter(usersController, checkJwt).routes();
 
 // Define PORT and wrap app in express:
 const PORT = process.env.PORT;
@@ -29,6 +36,13 @@ app.use(express.urlencoded({ extended: false }));
 
 // using the routers
 app.use("/users", usersRouter);
+// Auth0 Route that requires authentication:
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Application listening to port 3000");
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 class UsersRouter {
-  constructor(controller) {
+  constructor(controller, checkJwt) {
     this.controller = controller;
+    this.checkJwt = checkJwt;
   }
 
   routes() {
     router.get("/", this.controller.getAll.bind(this.controller));
-    router.get("/:userID", this.controller.getOneUser.bind(this.controller));
+    router.get(
+      "/:userID",
+      this.checkJwt,
+      this.controller.getOneUser.bind(this.controller)
+    );
 
     return router;
   }
